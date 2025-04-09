9e64f3fda0dd425652ad034095179decf8ef9d03
Axel Ortheden <axelort0607@gmail.com>
2025-04-10 00:56:27 +0200

    fixade users

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const session = require('express-session')
+const session = require('express-session');
 
 
 const app = express();
@@ -7,20 +7,22 @@ const app = express();
 
 
 
-const { createServer } = require("http")
-const { Server } = require("socket.io")
-const httpServer = createServer(app)
-const io = new Server(httpServer)
+const { createServer } = require("http");
+const { Server } = require("socket.io");
+const httpServer = createServer(app);
+const io = new Server(httpServer);
 
 const sessionMw = (session({
-  secret: 'keyboard cat',
-  resave: false,
-  saveUninitialized: true,
-  cookie: { secure: false }
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false }
 }))
 
+app.use(sessionMw);
 
-httpServer.listen(3000, _=>console.log("port 3000"));
+
+httpServer.listen(3000, _ => console.log("port 3000"));
 app.use(express.static("public"));
 
 
@@ -29,24 +31,39 @@ io.engine.use(sessionMw);
 io.on("connection", (socket) => {
     console.log("new connection...");
 
-    socket.emit("message", "welcome to the chat");
+    socket.emit("message", { user: "System", msg: "Welcome to the chat" });
 
-    io.emit("message", "a user has joined the chat")
-<
-    socket.on("disconnect", () => {
-        io.emit("message", "A user has left the chat")
-    })
+    io.emit("message", { user: "System", msg: "A user has joined the chat" });
     
-    socket.on("chatMessage", (msg) => {
-        io.emit("message", msg)
-    })
+        socket.on("disconnect", () => {
+            io.emit("message", "A user has left the chat");
+        })
 
+    socket.on("chatMessage", (msg) => {
+
+        if (socket.request.session.user) {
+
+            let user = socket.request.session.user;
+
+            io.emit("message", { user, msg });
+        }
+    })
+    console.log(socket.request.session);
 });
 
 
 
 
 app.get("/", (req, res) => {
-    res.sendFile(__dirname + "/chat.html")
+    if (req.session.user) {
+        res.sendFile(__dirname + "/chat.html");
+    } else {
+        res.send("<h1>Please login first</h1>");
+    }
 
+});
+
+app.get("/login/:user", (req, res) => {
+    req.session.user = req.params.user;
+    res.redirect("/?logged_in");
 });
--- public/client.js
+++ public/client.js
@@ -1,29 +1,32 @@
 const chatForm = document.getElementById("chatForm");
-const chatMessages = document.querySelector("chatMessages");
+const chatMessages = document.querySelector("#chatMessages");
+const chatBox = document.querySelector("#messageContainer");
 
 const socket = io();
 
 
-socket.on("message", message => {
-    console.log(message)
-    outputMessage(message)
-
-    chatFormmessage.scrollTop = chatFormmessage.scrollHeight
+socket.on("message", ({ user, msg }) => {
+    console.log(user, msg);
+    outputMessage(user, msg)
 });
 
 chatForm.addEventListener("submit", e => {
     e.preventDefault();
 
-    const msg = e.target.elements.msg.value;
-
+    const msgInput = e.target.elements.msg;
+    const msg = msgInput.value;
+    
     socket.emit("chatMessage", msg);
+    msgInput.value = "";
+
 })
 
-function outputMessage (message) {
+function outputMessage (user, message) {
     const div = document.createElement("div")
     div.classList.add("message")
-    div.innerHTML = `<p class="meta">User <span>${new Date().toLocaleTimeString()}</span></p>
+    div.innerHTML = `<p class="meta">${user} <span>${new Date().toLocaleTimeString()}</span></p>
         <p class="text">${message}</p>`
         document.getElementById("chatMessages").appendChild(div);
-        msg.value = "";
+    
+        
 }
